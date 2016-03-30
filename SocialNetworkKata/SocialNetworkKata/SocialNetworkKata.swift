d0731a1610a9a549ed3b0fcd92895651eafd6e01
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-03-30 20:07:40 +0100

    Create project skeleton

---
+++ SocialNetworkKata/SocialNetworkKata/SocialNetworkKata.swift
@@ -0,0 +1,5 @@
+class SocialNetworkKata {
+    func a(){
+        
+    }
+}
---
+++ SocialNetworkKata/SocialNetworkKata/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SocialNetworkKata
+//
+//  Created by Carlos Morera de la Chica on 30/03/2016.
+//  Copyright (c) 2016 Carlos Morera de la Chica. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, World!", terminator: "")
+
