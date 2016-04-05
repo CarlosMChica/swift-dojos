4b913a45d20f04ff7bb8b572968a65225c7d93d4
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-04-05 19:13:20 +0100

    Refactor test

--- SocialNetworkKata/SocialNetworkKataTest/SocialNetworkShould.swift
+++ SocialNetworkKata/SocialNetworkKataTest/SocialNetworkShould.swift
@@ -2,19 +2,33 @@ import XCTest
 
 class SocialNetworkShould: XCTestCase {
 
-  func test_execute_only_command_that_can_execute_the_given_action() {
-    let canExecuteCommand = ActionSpy(canExecute: true)
-    let canNotExecuteCommand = ActionSpy(canExecute: false)
-    let commands = [Action](arrayLiteral: canExecuteCommand, canNotExecuteCommand)
-    let socialNetwork = SocialNetwork(commands: commands)
-    let action = Input(input: "input")
+  func test_execute_only_action_that_can_execute_given_input() {
+    let canExecuteAction = givenCanExecuteAction()
+    let canNotExecuteAction = givenCanNotExecuteAction()
+    let socialNetwork = givenSocialNetworkWithActions(canExecuteAction, canNotExecuteAction)
+    let input = givenInput()
 
-    socialNetwork.execute(action)
+    socialNetwork.execute(input)
 
-    XCTAssertTrue(canExecuteCommand.executeCalled)
-    XCTAssertEqual(action, canExecuteCommand.executeInput)
-    XCTAssertFalse(canNotExecuteCommand.executeCalled)
+    XCTAssertTrue(canExecuteAction.executeCalled)
+    XCTAssertEqual(input, canExecuteAction.executeInput)
+    XCTAssertFalse(canNotExecuteAction.executeCalled)
   }
 
+  private func givenSocialNetworkWithActions(actions: Action...) -> SocialNetwork {
+    return SocialNetwork(actions: actions)
+  }
+
+  private func givenInput() -> Input {
+    return Input(input: "input")
+  }
+
+  private func givenCanExecuteAction() -> ActionSpy {
+    return ActionSpy(canExecute: true)
+  }
+
+  private func givenCanNotExecuteAction() -> ActionSpy {
+    return ActionSpy(canExecute: false)
+  }
 
 }
